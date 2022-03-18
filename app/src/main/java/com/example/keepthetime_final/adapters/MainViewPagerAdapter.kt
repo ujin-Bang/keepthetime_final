e68a96911c6b956f2995a3b55efaec9d4a772037
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-03-18 20:22:27 +0900

    #9 어댑터에 텝레이아웃 제목 표시완료

--- app/src/main/java/com/example/keepthetime_final/adapters/MainViewPagerAdapter.kt
+++ app/src/main/java/com/example/keepthetime_final/adapters/MainViewPagerAdapter.kt
@@ -8,6 +8,15 @@ import com.example.keepthetime_final.fragment.AppointmentListFragment
 import com.example.keepthetime_final.fragment.MyProfileFragment
 
 class MainViewPagerAdapter(fm: FragmentManager): FragmentPagerAdapter(fm) {
+
+    override fun getPageTitle(position: Int): CharSequence? {
+
+        return when(position){
+            0-> "내 약속목록 리스트"
+            else -> "내 프로필"
+        }
+    }
+
     override fun getCount() = 2
 
     override fun getItem(position: Int): Fragment {
