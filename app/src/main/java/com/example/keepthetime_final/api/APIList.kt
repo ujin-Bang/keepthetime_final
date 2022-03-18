7b89718fb910b00b6533c906cb7dc7dba207819d
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-03-18 12:49:03 +0900

    #4 인터페이스에 로그인 기능을 위함 함수 명세 작성

--- app/src/main/java/com/example/keepthetime_final/api/APIList.kt
+++ app/src/main/java/com/example/keepthetime_final/api/APIList.kt
@@ -1,4 +1,18 @@
 package com.example.keepthetime_final.api
 
+import org.json.JSONObject
+import retrofit2.Call
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.POST
+
 interface APIList {
+
+    @FormUrlEncoded
+    @POST("/user")
+    fun postRequestLogin(
+        @Field("email") email: String,
+        @Field("password")pw: String,
+    ): Call<JSONObject>
+
 }
--- app/src/main/java/com/example/keepthetime_final/api/ServerUtil.kt
+++ app/src/main/java/com/example/keepthetime_final/api/ServerUtil.kt
@@ -1,6 +1,7 @@
 package com.example.keepthetime_final.api
 
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 
 class ServerUtil {
@@ -8,10 +9,13 @@ class ServerUtil {
     companion object {
 
         private  var retrofit : Retrofit? = null
+        private  val BASE_URL = "http://keepthtime.xyz"
 
         fun getRetrofit() : Retrofit {
             if(retrofit == null) {
                 retrofit = Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
                     .build()
             }
             return retrofit!!
