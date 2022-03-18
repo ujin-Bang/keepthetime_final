3f26474ccff61745c8ee899c5c6d3937cd4d9ccb
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-03-18 12:59:12 +0900

    #4 Retrofit클래스 객체 + APIList 인터페이스 결합 => apiList인터페이스 변수 상속 처리.

--- app/src/main/java/com/example/keepthetime_final/BaseActivity.kt
+++ app/src/main/java/com/example/keepthetime_final/BaseActivity.kt
@@ -3,15 +3,22 @@ package com.example.keepthetime_final
 import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.keepthetime_final.api.APIList
+import com.example.keepthetime_final.api.ServerAPI
 
 abstract class BaseActivity: AppCompatActivity() {
 
     lateinit var mContext: Context
 
+    lateinit var apilist: APIList
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         mContext = this
+
+        val retrofit = ServerAPI.getRetrofit()
+        apilist = retrofit.create(APIList::class.java)
     }
 
     abstract fun setupEvents()
--- app/src/main/java/com/example/keepthetime_final/api/ServerAPI.kt
+++ app/src/main/java/com/example/keepthetime_final/api/ServerAPI.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 
-class ServerUtil {
+class ServerAPI {
 
     companion object {
 
