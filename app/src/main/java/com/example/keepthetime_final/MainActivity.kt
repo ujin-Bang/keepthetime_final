3640d6f1589d43518393dfc45de42e9a2f600e5d
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-03-18 11:02:05 +0900

    #1 dataBinting 세팅완료

--- app/src/main/java/com/example/keepthetime_final/MainActivity.kt
+++ app/src/main/java/com/example/keepthetime_final/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.keepthetime_final
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.keepthetime_final.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
