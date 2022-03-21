a81720d0e0840bdc863ff18a426f726dd8d2738e
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-03-21 17:22:42 +0900

    #15 통신시 토큰 자동으로 물려주도록 ServerAPI에서 토큰첨부 인터페이스 코드 추가

--- app/src/main/java/com/example/keepthetime_final/api/ServerAPI.kt
+++ app/src/main/java/com/example/keepthetime_final/api/ServerAPI.kt
@@ -1,5 +1,9 @@
 package com.example.keepthetime_final.api
 
+import android.content.Context
+import com.example.keepthetime_final.utils.ContextUtil
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -11,11 +15,30 @@ class ServerAPI {
         private  var retrofit : Retrofit? = null
         private  val BASE_URL = "https://keepthetime.xyz"
 
-        fun getRetrofit() : Retrofit {
+        fun getRetrofit(context: Context) : Retrofit {
             if(retrofit == null) {
+
+                val interceptor = Interceptor {
+                    with(it) {
+
+
+                        val newRequest = request().newBuilder()
+                            .addHeader("X-Http-Token", ContextUtil.getLoginUerToken(context))
+                            .build()
+
+                        proceed(newRequest)
+                    }
+
+                }
+
+                val myClient = OkHttpClient.Builder()
+                    .addInterceptor(interceptor)
+                    .build()
+
                 retrofit = Retrofit.Builder()
                     .baseUrl(BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
+                    .client(myClient)
                     .build()
             }
             return retrofit!!
