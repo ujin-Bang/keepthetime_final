3b521b4b53c937254e57349a730fed5b53367707
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-03-22 12:15:23 +0900

    #16 내 친구목록 액티비티 나누는 중 - 두번째 친구요청목록 페이지 작업 완료

--- app/src/main/java/com/example/keepthetime_final/fragment/RequestedUserFragment.kt
+++ app/src/main/java/com/example/keepthetime_final/fragment/RequestedUserFragment.kt
@@ -5,13 +5,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.keepthetime_final.R
+import com.example.keepthetime_final.adapters.RequestedUserRecyclerAdapter
 import com.example.keepthetime_final.databinding.FragmentRequestedUserBinding
+import com.example.keepthetime_final.datas.BasicResponse
+import com.example.keepthetime_final.datas.UserData
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class RequestedUserFragment:BaseFragment() {
 
     lateinit var binding: FragmentRequestedUserBinding
 
+    val mRequestUserList = ArrayList<UserData>()
+
+    lateinit var mAdapter : RequestedUserRecyclerAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -34,6 +44,34 @@ class RequestedUserFragment:BaseFragment() {
     }
 
     override fun setValues() {
+
+        getRequestedUserFromServer()
+        mAdapter = RequestedUserRecyclerAdapter(mContext, mRequestUserList)
+        binding.requestedUserRecylerView.adapter = mAdapter
+        binding.requestedUserRecylerView.layoutManager = LinearLayoutManager(mContext)
+
+    }
+
+    fun getRequestedUserFromServer(){
+        apiList.getRequestMyFriendList("requested").enqueue(object : Callback<BasicResponse>{
+            override fun onResponse(call: Call<BasicResponse>, response: Response<BasicResponse>) {
+
+                if (response.isSuccessful){
+
+                    mRequestUserList.clear()
+                    val br = response.body()!!
+                    mRequestUserList.addAll(br.data.friends)
+
+                    mAdapter.notifyDataSetChanged()
+
+                }
+            }
+
+            override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+
+            }
+
+        })
 
     }
 
