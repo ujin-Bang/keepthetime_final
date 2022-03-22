840216916f46ade1fee88fbea312e8ba0468ebb0
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-03-22 18:48:19 +0900

    #18 약속등록 화면 UI 작업중 - 날자 설정중 DatePickerDialog

--- app/src/main/java/com/example/keepthetime_final/EditAppointmentActivity.kt
+++ app/src/main/java/com/example/keepthetime_final/EditAppointmentActivity.kt
@@ -1,7 +1,10 @@
 package com.example.keepthetime_final
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.DatePicker
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.example.keepthetime_final.databinding.ActivityEditAppointmentBinding
 
@@ -12,9 +15,30 @@ class EditAppointmentActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_edit_appointment)
+        setupEvents()
+        setValues()
     }
 
     override fun setupEvents() {
+
+        binding.txtDate.setOnClickListener {
+
+            val dsl = object : DatePickerDialog.OnDateSetListener{
+                override fun onDateSet(p0: DatePicker?, year: Int, month: Int, day: Int) {
+
+                    Toast.makeText(mContext, "${year}년${month}월${day}일 선택함", Toast.LENGTH_SHORT).show()
+
+                }
+
+            }
+            val dpd = DatePickerDialog(
+                mContext,
+                dsl,
+                2022,
+                3,
+                22
+            ).show()
+        }
 
     }
 
