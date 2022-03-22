f5bee6d331fcc47a8edf11d23b4c1a0786df89f9
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-03-22 20:16:12 +0900

    #18 약속등록 화면 작업중 - 시간등록기능 완료  TimePickerDialog인터페이스 활용 + Calender + SimpleDateFormat

--- app/src/main/java/com/example/keepthetime_final/EditAppointmentActivity.kt
+++ app/src/main/java/com/example/keepthetime_final/EditAppointmentActivity.kt
@@ -1,9 +1,11 @@
 package com.example.keepthetime_final
 
 import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.DatePicker
+import android.widget.TimePicker
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.example.keepthetime_final.databinding.ActivityEditAppointmentBinding
@@ -41,9 +43,32 @@ class EditAppointmentActivity : BaseActivity() {
             val dpd = DatePickerDialog(
                 mContext,
                 dsl,
-                2022,
-                3,
-                22
+                mSelectedAppointmentDateTime.get(Calendar.YEAR),
+                mSelectedAppointmentDateTime.get(Calendar.MONTH),
+                mSelectedAppointmentDateTime.get(Calendar.DAY_OF_MONTH)
+
+            ).show()
+        }
+
+        binding.txtTime.setOnClickListener {
+            val tsl = object : TimePickerDialog.OnTimeSetListener{
+                override fun onTimeSet(p0: TimePicker?, hourOfDay: Int, minute: Int) {
+
+                    mSelectedAppointmentDateTime.set(Calendar.HOUR_OF_DAY, hourOfDay)
+                    mSelectedAppointmentDateTime.set(Calendar.MINUTE, minute)
+
+                    val sdf = SimpleDateFormat("a h시 m분")
+                    binding.txtTime.text = sdf.format(mSelectedAppointmentDateTime.time)
+
+                }
+
+            }
+            val tpd = TimePickerDialog(
+                mContext,
+                tsl,
+                18,
+                0,
+                false
             ).show()
         }
 
