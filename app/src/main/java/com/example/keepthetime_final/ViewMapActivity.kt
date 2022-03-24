286acc8d6593835ed32f0c38397d629b0184564d
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-03-24 17:44:57 +0900

    #21 모든 정거장 정모를 모아서 > PathOverlay가 지나가야할 좌표로 사용

--- app/src/main/java/com/example/keepthetime_final/ViewMapActivity.kt
+++ app/src/main/java/com/example/keepthetime_final/ViewMapActivity.kt
@@ -51,11 +51,19 @@ class ViewMapActivity : BaseActivity() {
             marker.map = naverMap
 
 //            Path 객체의 좌표를 설정 =>naverMap에 추가
-             val path = PathOverlay( )
+//             val path = PathOverlay( )
 
 //            path.coords => 출발지/ 도착지만 넣어서 대입하면? 일직선 열결
 
 //            출발지~ 도착지 사이의 정거장이 있다면 정거장들을 좌표로 추가.
+
+            val stationList = ArrayList<LatLng>()
+
+//            첫 좌표는 출발점
+            stationList.add(LatLng(mAppointment.start_latitude, mAppointment.start_longitude))
+
+//            거치게 되는 정거장들 목록을 >ODSayService로 받아서 추가
+
             val myODsayService = ODsayService.init(mContext,"R3sydUQ87JbAGzbchasHghy6awYJaDQnZJg3MdQ1QBE")
             myODsayService.requestSearchPubTransPath(
                 mAppointment.start_longitude.toString(),
@@ -74,6 +82,41 @@ class ViewMapActivity : BaseActivity() {
 
                         val resultObj = jonObj.getJSONObject("result")
                         Log.d("리졸트확인", resultObj.toString())
+
+                        val pathArr = resultObj.getJSONArray("path")
+                        val firstPathObj = pathArr.getJSONObject(0)
+
+                        Log.d("첫번째패스",firstPathObj.toString())
+
+                        val subPathArr = firstPathObj.getJSONArray("subPath")
+
+                        for(i in 0 until subPathArr.length()){
+                            val subPathObj = subPathArr.getJSONObject(i)
+
+                            if(!subPathObj.isNull("passStopList")){
+//                                도보가 아니어서 정거장 목록을 주는 경우
+                                val passStopListObj = subPathObj.getJSONObject("passStopList")
+                                val stationsArr = passStopListObj.getJSONArray("stations")
+
+                                for(j in 0 until stationsArr.length()){
+                                    val stationObj = stationsArr.getJSONObject(j)
+                                    val stationLat = stationObj.getString("y").toDouble()
+                                    val stationLng = stationObj.getString("x").toDouble()
+
+//                                    네이버 지도의 경로선에 그려줄 좌표 목록에 추가
+                                    stationList.add(LatLng(stationLat, stationLng))
+                                }
+                            }
+                        }
+
+//                        모든 정거장 목록이 추가되어있다.
+//                        마지막 경로선으로, 도착지를 추가.
+                        stationList.add(destLatLng)
+
+//                        경로선을 지도에 그려주자
+                        val path = PathOverlay()
+                        path.coords = stationList
+                        path.map = naverMap
                     }
 
                     override fun onError(p0: Int, p1: String?, p2: API?) {
