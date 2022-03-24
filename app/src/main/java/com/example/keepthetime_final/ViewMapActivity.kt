96187b689073bbe0614c5bb8575a79fa2a2788ce
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-03-24 17:06:18 +0900

    #21 길찾기 응답 확인  - 상세 파싱 진행중

--- app/src/main/java/com/example/keepthetime_final/ViewMapActivity.kt
+++ app/src/main/java/com/example/keepthetime_final/ViewMapActivity.kt
@@ -2,12 +2,18 @@ package com.example.keepthetime_final
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.databinding.DataBindingUtil
 import com.example.keepthetime_final.databinding.ActivityViewMapBinding
 import com.example.keepthetime_final.datas.AppointmentData
 import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.CameraUpdate
 import com.naver.maps.map.overlay.Marker
+import com.naver.maps.map.overlay.PathOverlay
+import com.odsay.odsayandroidsdk.API
+import com.odsay.odsayandroidsdk.ODsayData
+import com.odsay.odsayandroidsdk.ODsayService
+import com.odsay.odsayandroidsdk.OnResultCallbackListener
 
 class ViewMapActivity : BaseActivity() {
 
@@ -43,6 +49,39 @@ class ViewMapActivity : BaseActivity() {
             val marker = Marker()
             marker.position = destLatLng
             marker.map = naverMap
+
+//            Path 객체의 좌표를 설정 =>naverMap에 추가
+             val path = PathOverlay( )
+
+//            path.coords => 출발지/ 도착지만 넣어서 대입하면? 일직선 열결
+
+//            출발지~ 도착지 사이의 정거장이 있다면 정거장들을 좌표로 추가.
+            val myODsayService = ODsayService.init(mContext,"R3sydUQ87JbAGzbchasHghy6awYJaDQnZJg3MdQ1QBE")
+            myODsayService.requestSearchPubTransPath(
+                mAppointment.start_longitude.toString(),
+                mAppointment.start_latitude.toString(),
+                mAppointment.longitude.toString(),
+                mAppointment.latitude.toString(),
+                null,
+                null,
+                null,
+                object : OnResultCallbackListener{
+                    override fun onSuccess(p0: ODsayData?, p1: API?) {
+
+//                        JSONObject가 주는 것을 -> jsonObj에 받아서 > 내부 하나씩 파싱
+                        val jonObj = p0!!.json
+                        Log.d("대중교통 길찾기", jonObj.toString())
+
+                        val resultObj = jonObj.getJSONObject("result")
+                        Log.d("리졸트확인", resultObj.toString())
+                    }
+
+                    override fun onError(p0: Int, p1: String?, p2: API?) {
+
+                    }
+
+                }
+            )
         }
 
     }
