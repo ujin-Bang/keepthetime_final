f8d0a66966af0f84963789bb7acfab23e2b39997
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-03-25 18:01:00 +0900

    #26 임시작업: 선택한 이미지의 경로(Uri)를 바로 이미지뷰에 반영  -서버 전송은 아직안함

--- app/src/main/java/com/example/keepthetime_final/fragment/MyProfileFragment.kt
+++ app/src/main/java/com/example/keepthetime_final/fragment/MyProfileFragment.kt
@@ -1,5 +1,6 @@
 package com.example.keepthetime_final.fragment
 
+import android.app.Activity
 import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
@@ -107,4 +108,20 @@ class MyProfileFragment: BaseFragment() {
 
         })
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQ_CODE_GALLERY){
+            if (requestCode == Activity.RESULT_OK) {
+//                data? 변수가 선택된 사진에 대한 정보를 가지고 있다.
+                val selectedImageUri = data?.data!! //선택한 사진에 찾아갈 경로(Uri) 받아내기
+
+//                임시 : 선택한 사진을 이미지뷰에 반영
+                Glide.with(mContext).load(selectedImageUri).into(binding.imgProfile)
+            }
+        }
+
+    }
+
 }
