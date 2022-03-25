32e91f96fe8f9766810db22da65d6dbcc4900aaa
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-03-25 10:52:18 +0900

    #23 커스텀 액션바 적용

--- app/src/main/java/com/example/keepthetime_final/BaseActivity.kt
+++ app/src/main/java/com/example/keepthetime_final/BaseActivity.kt
@@ -2,7 +2,9 @@ package com.example.keepthetime_final
 
 import android.content.Context
 import android.os.Bundle
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import com.example.keepthetime_final.api.APIList
 import com.example.keepthetime_final.api.ServerAPI
 
@@ -19,8 +21,26 @@ abstract class BaseActivity: AppCompatActivity() {
 
         val retrofit = ServerAPI.getRetrofit(mContext)
         apilist = retrofit.create(APIList::class.java)
+
+        supportActionBar?.let {
+            setCustomActionBar()
+
+        }
     }
 
     abstract fun setupEvents()
     abstract fun setValues()
+
+    fun setCustomActionBar() {
+
+        val defaultActionBar = supportActionBar!!
+
+        defaultActionBar.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
+
+        defaultActionBar.setCustomView(R.layout.my_custom_action_bar)
+
+        val toolBar = defaultActionBar.customView.parent as Toolbar
+        toolBar.setContentInsetsAbsolute(0,0)
+
+    }
 }
