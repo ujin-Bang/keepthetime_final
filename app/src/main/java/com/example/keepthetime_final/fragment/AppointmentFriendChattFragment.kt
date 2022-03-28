81cbb8bf23f96755d5ea30ba9422a38bcf0d616c
uin bang <qkddnwls14@gmail.com>
2022-03-28 10:56:16 +0900

    #27 DB의 내용 변경 이벤트를 앱에서 감지  - 총 몇개의 메세지가 들어있는지 확인

--- app/src/main/java/com/example/keepthetime_final/fragment/AppointmentFriendChattFragment.kt
+++ app/src/main/java/com/example/keepthetime_final/fragment/AppointmentFriendChattFragment.kt
@@ -60,14 +60,14 @@ class AppointmentFriendChattFragment: BaseFragment() {
             val inputContent = binding.edtContent.text.toString()
 
 //            임시: DB의 하위 항목으로 => message 항목 => 0번 항목의 => content항목: 입력내용
-            realtimeDB.getReference("message").child("0").child("content").setValue(inputContent)
+            realtimeDB.getReference("message").child(messageCount.toString()).child("content").setValue(inputContent)
 //            추가 기록: 현재 시간값을 "2022년 3월 5일 오후 5:05" 양식으로 기록
 
             val now = Calendar.getInstance()
             val sdf = SimpleDateFormat("yyyy년 M월 d일 a h:mm")
             val nowStr = sdf.format(now.time)
 
-            realtimeDB.getReference("message").child("0").child("createdAt").setValue(nowStr)
+            realtimeDB.getReference("message").child(messageCount.toString()).child("createdAt").setValue(nowStr)
         }
 
     }
